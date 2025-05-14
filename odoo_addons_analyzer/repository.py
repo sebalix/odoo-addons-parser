5db7b5489bf55e5aac17b449571715cd3709d1fb
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-05-14 08:30:09 +0200

    Extract Odoo model definitions

--- odoo_addons_analyzer/repository.py
+++ odoo_addons_analyzer/repository.py
@@ -13,13 +13,15 @@ class RepositoryAnalysis:
         folder_path,
         languages=("Python", "XML", "CSS", "JavaScript"),
         name=None,
+        scan_models=True,
     ):
         self.folder_path = folder_path
         self.languages = languages
         self.name = os.path.basename(folder_path) if name is None else name
+        self._scan_models = scan_models
 
     @property
-    def modules_paths(self):
+    def module_paths(self):
         pattern1 = os.path.join(self.folder_path, "*", "__manifest__.py")
         pattern2 = os.path.join(self.folder_path, "*", "__openerp__.py")
         file_paths = glob.glob(pattern1) + glob.glob(pattern2)
@@ -27,10 +29,13 @@ class RepositoryAnalysis:
 
     def to_dict(self):
         data = {}
-        for module_path in self.modules_paths:
+        for module_path in self.module_paths:
             module = os.path.basename(module_path)
             analysis = ModuleAnalysis(
-                module_path, languages=self.languages, repo_analysis=self
+                module_path,
+                languages=self.languages,
+                repo_analysis=self,
+                scan_models=self._scan_models,
             )
             data[module] = analysis.to_dict()
         return data
