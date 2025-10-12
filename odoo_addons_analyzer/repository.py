40f820a69c9a1fc223e45cc221cd70310df1f490
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-10-12 13:08:33 +0200

    Repository: leverage multiprocessing with 'workers' parameter

--- odoo_addons_analyzer/repository.py
+++ odoo_addons_analyzer/repository.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Camptocamp SA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
+import multiprocessing
 import os
 import pathlib
 import typing
@@ -14,11 +15,13 @@ class RepositoryAnalysis:
         folder_path: typing.Union[str, os.PathLike],
         languages: tuple[str, ...] = ("Python", "XML", "CSS", "JavaScript"),
         name: typing.Optional[str] = None,
+        workers: int = 0,
         scan_models: bool = True,
     ):
         self.folder_path = pathlib.Path(folder_path).resolve()
         self.languages = languages
         self.name = self.folder_path.name if name is None else name
+        self.workers = workers
         self._scan_models = scan_models
 
     @property
@@ -28,15 +31,27 @@ class RepositoryAnalysis:
             | set(map(lambda fp: fp.parent, self.folder_path.glob("*/__openerp__.py")))
         )
 
+    def _scan_module(self, module_path):
+        analysis = ModuleAnalysis(
+            module_path,
+            languages=self.languages,
+            repo_analysis=self,
+            scan_models=self._scan_models,
+        )
+        return analysis.to_dict()
+
     def to_dict(self) -> dict:
         data = {}
-        for module_path in self.module_paths:
-            module = module_path.name
-            analysis = ModuleAnalysis(
-                module_path,
-                languages=self.languages,
-                repo_analysis=self,
-                scan_models=self._scan_models,
-            )
-            data[module] = analysis.to_dict()
+        # Multiworkers
+        if self.workers:
+            with multiprocessing.Pool(self.workers) as pool:
+                results = pool.map(self._scan_module, self.module_paths)
+
+            for module_data in results:
+                data[module_data["name"]] = module_data
+        # Monoprocess
+        else:
+            for module_path in self.module_paths:
+                module = module_path.name
+                data[module] = self._scan_module(module_path)
         return data
